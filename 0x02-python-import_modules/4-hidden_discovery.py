038c66e061f1d21b5e7511d82c2f473ee884af83
leehomeecky <meecky4christ@gmail.com>
2023-06-08 22:17:39 +0100

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     import hidden_4
 
     allNames = dir(hidden_4)
-    for name in all:
+    for name in allNames:
         if name[:2] != "__":
             print(name)
