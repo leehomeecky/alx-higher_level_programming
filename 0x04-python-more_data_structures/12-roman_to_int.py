c294a3dd6587193c4189a68c22260fcac965c79b
leehomeecky <meecky4christ@gmail.com>
2023-06-15 04:24:20 +0100

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if (not isinstance(roman_string, str) or
+            roman_string is None):
+        return (0)
+
+    romanSwitch = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+    }
+    num = 0
+
+    for i in range(len(roman_string)):
+        if romanSwitch.get(roman_string[i], 0) == 0:
+            return (0)
+
+        if (i != (len(roman_string) - 1) and
+                romanSwitch[roman_string[i]] < romanSwitch[roman_string[i + 1]]):
+                num += romanSwitch[roman_string[i]] * -1
+        else:
+            num += romanSwitch[roman_string[i]]
+    return (num)
