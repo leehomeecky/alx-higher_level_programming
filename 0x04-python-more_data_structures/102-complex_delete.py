21d47f7f2132e880bcf917a7a83671b72e028abe
leehomeecky <meecky4christ@gmail.com>
2023-06-15 04:38:21 +0100

    Advance tasks

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    while value in a_dictionary.values():
+        for key, item in a_dictionary.items():
+            if item == value:
+                del a_dictionary[key]
+                break
+
+    return (a_dictionary)
