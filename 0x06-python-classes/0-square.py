126addee521f59238ca9edc5a0f160067ba3952c
leehomeecky <meecky4christ@gmail.com>
2023-06-27 22:11:49 +0100

    cleanup

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,4 +3,5 @@
 
 
 class Square:
+    """a square empty class."""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
 
 class Square:
+    """a quare class."""
 
     def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
