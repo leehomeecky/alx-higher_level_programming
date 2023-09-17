1906d1b5f2ee868047d60b0a612cb7e4faac9001
leehomeecky <meecky4chist@gmail.com>
2023-09-17 07:53:29 +0100

    solution

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-""" script that takes name of a state and lists all cities """
-
+"""  list all cites with its states"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-   db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-   cur = db.cursor()
-   cur.execute("SELECT cities.name "
-               "FROM cities WHERE cities.state_id = "
-               "(SELECT id FROM states WHERE name LIKE BINARY %s)"
-               " ORDER BY cities.id", (sys.argv[4], ))
-   result = [rowVal[0] for rowVal in cur.fetchall()]
-   print(*result, sep=", ")
+    value = sys.argv[4]
+    cur = db.cursor()
+    cur.execute("SELECT cities.name "
+                "FROM cities WHERE cities.state_id = "
+                "(SELECT id FROM states WHERE name LIKE BINARY %s)"
+                " ORDER BY cities.id", (value, ))
+
+    val = list(rowVal[0] for rowVal in cur.fetchall())
+    print(*val, sep=", ")
+    cur.close()
+    db.close()
