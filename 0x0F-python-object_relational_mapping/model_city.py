0aaa054c8ed8e8dfd6f6f176c4d08a14b64fca31
leehomeecky <meecky4chist@gmail.com>
2023-09-17 07:43:59 +0100

    fix

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# Defines a City model.
-# Inherits from SQLAlchemy Base and links to the MySQL table cities.
+""" Defines a City model. """
 
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
