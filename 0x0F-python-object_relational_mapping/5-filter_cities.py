3138164fe38cf6fa25dfa1eb1fbb9c9893480845
leehomeecky <meecky4chist@gmail.com>
2023-09-17 06:52:31 +0100

    fix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -4,12 +4,12 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+   db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    c = db.cursor()
-    c.execute("SELECT cities.name "
-                "FROM cities WHERE cities.state_id = "
-                "(SELECT id FROM states WHERE name LIKE BINARY %s)"
-                " ORDER BY cities.id", (sys.argv[4], ))
-    result = list(rowVal[0] for rowVal in cur.fetchall())
-    print(*result, sep=", ")
+   cur = db.cursor()
+   cur.execute("SELECT cities.name "
+               "FROM cities WHERE cities.state_id = "
+               "(SELECT id FROM states WHERE name LIKE BINARY %s)"
+               " ORDER BY cities.id", (sys.argv[4], ))
+   result = list(rowVal[0] for rowVal in cur.fetchall())
+   print(*result, sep=", ")
