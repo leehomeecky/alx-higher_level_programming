8d23720cb8c947e14b7121039db5a20ae956644d
leehomeecky <meecky4chist@gmail.com>
2023-10-24 20:49:02 +0100

    test fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 const request = require("request");
-const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
 request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    console.log(JSON.parse(response.body).title);
+  } else {
+    console.log("Error code: " + response.statusCode);
+  }
 });
